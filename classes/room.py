fcb2db3d627e50dbf0614ff55d9eaa24b2ce1cc0
Andrew High <andrewhigh4@hotmail.com>
2021-04-09 16:24:23 +0100

    check_guest_into_room test passing

--- classes/room.py
+++ classes/room.py
@@ -3,9 +3,17 @@ class Room:
         self.room_name = room_name
         self.capacity = capacity
         self.song_library = []
+        self.guest_list = []
 
     def check_song_library(self):
         return len(self.song_library)
 
     def add_song_to_library(self, song):
         self.song_library.append(song)
+
+    def check_guest_list(self):
+        return len(self.guest_list)
+    
+    def check_guest_into_room(self, guest):
+        self.guest_list.append(guest)
+
--- tests/room_test.py
+++ tests/room_test.py
@@ -2,11 +2,13 @@ import unittest
 
 from classes.room import Room
 from classes.song import Song
+from classes.guest import Guest
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
         self.room_1 = Room("Lizard Lounge", 6)
         self.song_1 = Song("Killer Queen")
+        self.guest_1 = Guest("Freddie Mercury", "Bohemian Rhapsody")
 
     def test_room_has_name(self):
         self.assertEqual("Lizard Lounge", self.room_1.room_name)
@@ -14,3 +16,7 @@ class TestRoom(unittest.TestCase):
     def test_add_song_to_library(self):
         self.room_1.add_song_to_library(self.song_1)
         self.assertEqual(1, self.room_1.check_song_library())
+
+    def test_check_guest_into_room(self):
+        self.room_1.check_guest_into_room(self.guest_1)
+        self.assertEqual(1, self.room_1.check_guest_list())
