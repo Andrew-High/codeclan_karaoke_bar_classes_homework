8f87203f99d0044a57303a5f5c28ea8045a8de9c
Andrew High <andrewhigh4@hotmail.com>
2021-04-09 16:02:29 +0100

    guest class set up and first test passing

---
+++ classes/guest.py
@@ -0,0 +1,5 @@
+class Guest:
+    def __init__(self, guest_name, favourite_song):
+        self.guest_name = guest_name
+        self.favourite_song = favourite_song
+        self.currently_singing = []
--- tests/guest_test.py
+++ tests/guest_test.py
@@ -1,3 +1,10 @@
 import unittest
 
 from classes.guest import Guest
+
+class TestGuest(unittest.TestCase):
+    def setUp(self):
+        self.guest_1 = Guest("Freddie Mercury", "Bohemian Rhapsody")
+
+    def test_guest_has_name(self):
+        self.assertEqual("Freddie Mercury", self.guest_1.guest_name)
