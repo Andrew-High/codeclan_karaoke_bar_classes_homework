d1a17e1d6b555c6d4483149dc224f10459a8e5eb
Andrew High <andrewhigh4@hotmail.com>
2021-04-09 16:10:21 +0100

    add song to library test working

--- classes/room.py
+++ classes/room.py
@@ -3,3 +3,9 @@ class Room:
         self.room_name = room_name
         self.capacity = capacity
         self.song_library = []
+
+    def check_song_library(self):
+        return len(self.song_library)
+
+    def add_song_to_library(self, song):
+        self.song_library.append(song)
--- tests/room_test.py
+++ tests/room_test.py
@@ -10,3 +10,7 @@ class TestRoom(unittest.TestCase):
 
     def test_room_has_name(self):
         self.assertEqual("Lizard Lounge", self.room_1.room_name)
+
+    def test_add_song_to_library(self):
+        self.room_1.add_song_to_library(self.song_1)
+        self.assertEqual(1, self.room_1.check_song_library())
