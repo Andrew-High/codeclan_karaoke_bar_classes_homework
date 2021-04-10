0be60856ccc02326bd5e9668cc9bdf79e61b021d
Andrew High <andrewhigh4@hotmail.com>
2021-04-10 14:01:41 +0100

    added check for favourite song method to guest class

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -1,13 +1,22 @@
 import unittest
 
 from classes.guest import Guest
+from classes.room import Room
+from classes.song import Song
 
 class TestGuest(unittest.TestCase):
     def setUp(self):
         self.guest_1 = Guest("Freddie Mercury", "Killer Queen", 30)
+        self.room_1 = Room("Lizard Lounge", 4, 10)
+        self.song_1 = Song("Killer Queen")
 
     def test_guest_has_name(self):
         self.assertEqual("Freddie Mercury", self.guest_1.guest_name)
 
     def test_guest_favourite_song(self):
         self.assertEqual("Killer Queen", self.guest_1.favourite_song)
+
+    def test_response_when_favourite_song_present_in_song_library(self, room):
+        self.room_1.add_song_to_library(self.song_1)
+        self.room_1.check_guest_into_room(self.guest_1)
+        self.assertEqual("Yeah! They have Killer Queen, I'm going to sing that!", self.check_if_favourite_song_present())
