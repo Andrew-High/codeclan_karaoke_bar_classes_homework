67f3bb90fa93247822c42df161392bdcd219f344
Andrew High <andrewhigh4@hotmail.com>
2021-04-10 13:26:45 +0100

    added test for favourite song

--- tests/guest_test.py
+++ tests/guest_test.py
@@ -4,7 +4,10 @@ from classes.guest import Guest
 
 class TestGuest(unittest.TestCase):
     def setUp(self):
-        self.guest_1 = Guest("Freddie Mercury", "Bohemian Rhapsody")
+        self.guest_1 = Guest("Freddie Mercury", "Killer Queen")
 
     def test_guest_has_name(self):
         self.assertEqual("Freddie Mercury", self.guest_1.guest_name)
+
+    def test_guest_favourite_song(self):
+        self.assertEqual("Killer Queen", self.guest_1.favourite_song)
--- tests/room_test.py
+++ tests/room_test.py
@@ -8,7 +8,7 @@ class TestRoom(unittest.TestCase):
     def setUp(self):
         self.room_1 = Room("Lizard Lounge", 6)
         self.song_1 = Song("Killer Queen")
-        self.guest_1 = Guest("Freddie Mercury", "Bohemian Rhapsody")
+        self.guest_1 = Guest("Freddie Mercury", "Killer Queen")
 
     def test_room_has_name(self):
         self.assertEqual("Lizard Lounge", self.room_1.room_name)
